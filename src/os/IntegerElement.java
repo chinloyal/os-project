e5c07ca1c306ac3ba92fa176ba2a3f91b2718079
chinloyal <jordain7@gmail.com>
2018-11-20 11:53:58 -0500

    Added integer element

---
+++ src/os/IntegerElement.java
@@ -0,0 +1,21 @@
+package os;
+
+public class IntegerElement {
+	private String key;
+	private int value;
+	
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	
+}
